bfcffdc24b4a8c3be3cf15b505ad6524e10de1ae
Manuel Ucles <78464298+mucles@users.noreply.github.com>
2024-07-21 16:56:32 -0700

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls","docker_compose_language_service","dockerls","clangd","cmake","eslint","gopls","graphql","helm_ls","jsonls","pyright","rust_analyzer","sqlls", "tailwindcss","terraform_lsp","tsserver","ocamllsp", "marksman"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +15,4 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+
