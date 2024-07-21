7de4d9565289015525ae3ce403f9a28b9d0bfc0b
Manuel Ucles <78464298+mucles@users.noreply.github.com>
2024-07-21 16:57:28 -0700

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,21 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    -- event = 'BufWritePre' -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
   -- {
   -- 	"williamboman/mason.nvim",
   -- 	opts = {
@@ -30,7 +30,7 @@ return {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
+  -- 			"vim", "lua", "vimdoc
   --      "html", "css"
   -- 		},
   -- 	},
